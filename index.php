999d689537dd8b2f35ed9299c496cb0731f8113e
swapnajajadhav <swapnaja.jadhav@augustwebsys.com>
2016-10-05 11:23:50 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta type="utf-8">
+<link href="css/style.css">
+ <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="js/myscript.js"></script>
+<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+</head>
+<body>
+<p id="map" style="height:300px; width:300px;">this is sample code</p>
+<button>Stop</button>
+</body>
+</html>
